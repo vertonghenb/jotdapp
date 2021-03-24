4efee44a54a004abd2e381217dcf3767c06fa732
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-03-24 12:43:07 +0100

    [Joke-List] Scaffold
    - [ x ] cd src/app
    - [ x ] ng g c ./joke/JokeList --module=joke
    - Don't forget to export the list component!

--- src/app/joke/joke.module.ts
+++ src/app/joke/joke.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from "./../material/material.module";
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [JokeListComponent],
   imports: [
     CommonModule,
     MaterialModule
-  ]
+  ],
+  exports: [JokeListComponent]
 })
 export class JokeModule { }
